5dbe86ae77d26121d402b62d7bba0240b4ac4f09
Joao Paulo Lethier <jplethier@gmail.com>
2019-10-06 20:51:58 -0300

    sidekiq admin routes and configuration;

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  require 'sidekiq/web'
+  # simple route configuration
+  mount Sidekiq::Web => '/sidekiq'
+
+  # route configuration with devise authentication
+  # authenticate :user do
+  #   mount Sidekiq::Web => '/sidekiq'
+  # end
+
+  # route configuration with devise authentication and permission
+  # authenticate :user, lambda { |u| u.admin? } do
+  #   mount Sidekiq::Web => '/sidekiq'
+  # end
 end
